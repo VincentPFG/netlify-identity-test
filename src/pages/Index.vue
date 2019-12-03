27cbabae00051bf8c58f4042875c45b1792b1161
VincentPFG <vincentgiraud@live.fr>
2019-12-03 14:46:00 +0100

    widget

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,11 @@
 <template lang='pug'>
 Layout
-    
+    #data-netlify-identity-menu
+    #data-netlify-identity-button Login with Netlify Identity
 </template>
 
 <script lang='coffee'>
-
+import 'netlify-identity-widget'
 </script>
 
 <style lang="sass">
